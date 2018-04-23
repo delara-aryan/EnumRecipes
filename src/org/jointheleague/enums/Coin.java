47bfac6c4a2e319574bf73ee1682e5ea4ba0b9c4
delara-aryan <delara.aryan@gmail.com>
2018-04-22 17:26:05 -0700

    enums: coin and direction

--- src/org/jointheleague/enums/Coin.java
+++ src/org/jointheleague/enums/Coin.java
@@ -5,26 +5,25 @@ import java.util.Map;
 
 public enum Coin {
 
+    PENNY(1), NICKEL(5), DIME(10), QUARTER(25);
 
-    // 1. There are 4 coins: PENNY, NICKEL, DIME, QUARTER. Add them to this enum.
+    private final int value;
 
-    // 2. Each coin has a value of type int, which never changes; 1 for PENNY, 5 for NICKEL, etc. 
-    // Create a constructor takes the value of the coin as argument. Apply this constructor in 
-    // Step 1.
+    Coin(int value) {
+        this.value = value;
+    }
 
     public int getValue() {
-	// 3. Fill in. This method should return the value of the coin.
+        return value;
     }
-    
     
     public static Map<Coin, Integer> getChange(int money) {
 	Map<Coin, Integer> change = new HashMap<>();
-	// 4. Fill in. The returned value is a map that tells you how many of each coin should be 
-	// returned in exchange for money. As few coins as possible should be returned. 
-	// For example, if money == 15, then 1 DIME and 1 NICKEL should be 
-	// returned rather than 2 NICKEL(s) and 5 PENNY(s). 
-	
-
+	for (int i = 3; i >= 0; i--) {
+	    Coin c = Coin.values()[i];
+	    change.put(c, money/c.getValue());
+	    money = money % c.getValue();
+    }
 	return change;
     }
 
